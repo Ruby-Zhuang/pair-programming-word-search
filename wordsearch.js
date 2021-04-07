2a2a34cc9489d56f75d8524827f1374034b8ebc4
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2021-04-07 17:40:33 -0400

    Update wordsearch to include empty array case, horizontal and vertical check, backwards check

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,39 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  // Empty array case
+  if (letters.length === 0) return false;
 
-module.exports = wordSearch
+  // Reverse word to check for backwards case
+  let wordReversed = word.split('').reverse().join('');
+
+  // Transpose letters for vertical case
+  const vertical = transpose(letters);
+
+  // Check horizontally
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (const line of horizontalJoin) {
+    if (line.includes(word) || line.includes(wordReversed)) return true;
+  }
+
+  // Check vertically
+  const verticalJoin = vertical.map(ls => ls.join(''));
+  for (const line of verticalJoin) {
+    if (line.includes(word) || line.includes(wordReversed)) return true;
+  }
+
+  return false;
+};
+
+// Transpose function
+const transpose = (matrix) => {
+  const result = []; // Initialize empty array
+
+  for (let x = 0; x < matrix.length; x++) {
+    for (let y = 0; y < matrix[x].length; y++) {
+      if (x === 0) result[y] = []; // Initialize each row of transposed matrix (# of rows in transposed should be # of columns in original matrix)
+      result[y][x] = matrix[x][y];
+    }
+  }
+  return result;
+};
+
+module.exports = wordSearch;
